ea499b214dba867a3f3db2e0aeb679e7b02aff7b
Noob1545 <emirhantopal1545@gmail.com>
2024-02-18 14:21:33 +0300

    deneme

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const multer = require("multer")
+
+const app = express()
+
+let fileName;
+
+const storage = multer.diskStorage({
+    destination:function(request,file,callback){
+        callback(null,"uploads/")
+    },
+    filename: function(request,file,callback){
+        callback(null,file.originalname)
+        fileName = file.originalname
+        console.log(fileName);
+    }
+})
+
+const upload = multer({storage: storage})
+
+app.get("/",function(request,response){
+    response.sendFile(__dirname+"/index.html")
+})
+
+app.use("/uploads",express.static("uploads"))
+
+app.post("/uploadFile",upload.single("file"),function(request,response){
+    response.send(`
+        <script>
+            window.location.href = 'http://localhost:3000/uploads/${fileName}'
+        </script>
+    `)
+})
+
+app.listen(3000,function(){
+    console.log("sunucu http://localhost:3000 de dinleniyor!");
+})
